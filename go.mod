12d1d6f443bbc82846d44c1956fcae698d322eb0
Alireza Saadatnia <alirezasn3@gmail.com>
2024-07-12 15:07:01 +0330

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dante-ui
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"slices"
+)
+
+func main() {
+	// check dante version
+	// check dante service file
+	// check dante config file
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	conn, err := listener.Accept()
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	for {
+		bytes, err := reader.ReadBytes('\n')
+		if err != nil {
+			panic(err)
+		}
+		percentIndex := slices.Index(bytes, '%')
+		if percentIndex < 0 {
+			continue
+		}
+		atIndex := slices.Index(bytes[percentIndex:], '@')
+		if atIndex < 0 {
+			continue
+		}
+		openParenIndex := slices.Index(bytes[atIndex:], '(')
+		if openParenIndex < 0 {
+			continue
+		}
+		closeParenIndex := slices.Index(bytes[openParenIndex:], ')')
+		if closeParenIndex < 0 {
+			continue
+		}
+		fmt.Printf("%s -> %s bytes\n", bytes[percentIndex:atIndex], bytes[openParenIndex:closeParenIndex])
+	}
+}
