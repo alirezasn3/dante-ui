591a4e34dafadd21e4f7f2ade021560a28642724
Alireza Saadatnia <alirezasn3@gmail.com>
2024-07-20 12:50:47 +0330

    fixed curl command

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-curl -O https://github.com/alirezasn3/dante-ui/releases/download/v1.0.0/dante-ui-linux-amd64
+curl -LO https://github.com/alirezasn3/dante-ui/releases/download/v1.0.0/dante-ui-linux-amd64
 mv dante-ui-linux-amd64 dante-ui
 chmod +x dante-ui
 cp dante-ui.service /etc/systemd/system/
